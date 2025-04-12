c4db1a3f8a5f804bce5c32f1b332161502fa2b4a
valek228123 <valentinveremchuk.2007@gmail.com>
2025-04-12 17:16:39 +0300

    asdfas

--- test.py
+++ test.py
@@ -3,3 +3,9 @@ def sum(a:int, b:int) -> int:
     return a+b
 print(sum(5,10))
 print("asdasd")
+def ftuct():
+    while True:
+        yield "Its the first of the month"
+for i in ftuct():
+    print(i)
+
